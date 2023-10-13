ef6197e048e31bf0469502ab85c837f7170ba052
Kanro-Code <tervelde.thijs@gmail.com>
2023-10-13 09:14:38 +0200

    { Feature } Middleware added

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,19 @@
 import express from 'express';
 import cors from 'cors';
-import morgan from 'morgan';
 import urlRouter from './routers/urlRouter';
 import redirectRouter from './routers/redirectRouter';
+import middleware from './routers/middleware';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(middleware.requestLogger);
 
 app.use('/', redirectRouter);
 app.use('/api/url', urlRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 export default app;
---
+++ src/routers/middleware.ts
@@ -0,0 +1,27 @@
+import morgan from 'morgan';
+import { NextFunction, Request, Response } from 'express';
+
+const requestLogger = morgan('dev');
+
+const errorHandler = (
+	error: Error,
+	_request: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	console.error(error.message);
+
+	// TODO: Actually handle errors
+
+	return next(error);
+};
+
+const unknownEndpoint = (_request: Request, res: Response) => {
+	res.status(404).send({ error: 'unknown endpoint' });
+};
+
+export default {
+	requestLogger,
+	errorHandler,
+	unknownEndpoint,
+};
