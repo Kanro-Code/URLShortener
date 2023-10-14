3f4717138f915429b0d495fa71246f53533079cf
Kanro-Code <tervelde.thijs@gmail.com>
2023-10-14 08:36:29 +0200

    no message

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import app from './app.js';
 import config from './utils/config.js';
 
 app.listen(config.port, () => {
-	console.log(`Server is running on port ${config.port}`);
+	console.log(`Server is running on port http://localhost:${config.port}`);
 });
